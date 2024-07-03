7aec6ca5f30377f4241089e954bbb842a90e8b09
sytone <github@sytone.com>
2024-07-03 14:26:27 -0700

    feat: cleanup

--- Get-BootstrapAndRun.ps1
+++ Get-BootstrapAndRun.ps1
@@ -1,19 +1,15 @@
 Write-Output 'Creating system-bootstrap folder.'
 New-Item -Path 'C:\Temp\system-bootstrap' -ItemType Directory -Force
-
 Write-Output 'Downloading system-bootstrap files.'
-
 $files = @(
     'Start-SystemBootstrap.ps1',
     'basesystem.dsc.yaml',
     'Start-SystemSetup.ps1'
 )
-
 foreach ($file in $files) {
     $scriptDownload = Invoke-WebRequest "https://raw.githubusercontent.com/sytone/system-bootstrap/main/$file"
     Write-Output "Downloading $file to C:\Temp\system-bootstrap\$file"
     $scriptDownload.Content | Out-File -FilePath "C:\Temp\system-bootstrap\$file" -Force
 }
-
 Write-Output 'Running Start-SystemBootstrap.ps1'
 & 'C:\Temp\system-bootstrap\Start-SystemBootstrap.ps1'
