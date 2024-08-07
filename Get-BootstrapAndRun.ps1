08c46ab92e1317036bf1de46a3261e6c985bbb44
Sytone <github@sytone.com>
2024-08-07 16:28:20 -0700

    Update Get-BootstrapAndRun.ps1

--- Get-BootstrapAndRun.ps1
+++ Get-BootstrapAndRun.ps1
@@ -1,13 +1,22 @@
 #Requires -PSEdition Desktop
 # Bootstrap should be run on windows powershell which is the desktop version.
 
+Write-Output ' __                _                      '
+Write-Output '(_    __|_ _ ._ _ |_) _  __|_ __|_.__.._  '
+Write-Output '__)\/_> |_(/_| | ||_)(_)(_)|__> |_|(_||_) '
+Write-Output '   /                                  |   '
+Write-Output ''
 Write-Output 'Creating system-bootstrap folder.'
+
 $bootstrapFolder = "$env:temp\system-bootstrap"
 New-Item -Path $bootstrapFolder -ItemType Directory -Force | Out-Null
+
 if(-not (Test-Path $bootstrapFolder)) {
     Write-Output "Failed to create system-bootstrap folder '$bootstrapFolder'."
     return
 }
+
+Write-Output "Folder created at: '$bootstrapFolder'"
 
 Write-Output 'Downloading system-bootstrap files.'
 $files = @(
@@ -15,10 +24,13 @@ $files = @(
     'basesystem.dsc.yaml',
     'Start-SystemSetup.ps1'
 )
+
 foreach ($file in $files) {
     $scriptDownload = Invoke-WebRequest "https://raw.githubusercontent.com/sytone/system-bootstrap/main/$file"
     Write-Output "Downloading $file to $bootstrapFolder\$file"
     $scriptDownload.Content | Out-File -FilePath "$bootstrapFolder\$file" -Force
 }
-Write-Output 'Running Start-SystemBootstrap.ps1'
+
+Write-Output "Running $bootstrapFolder\Start-SystemBootstrap.ps1"
+
 & "$bootstrapFolder\Start-SystemBootstrap.ps1"
